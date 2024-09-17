14cac363465c5d1c0dc40ecbf56f57d054a446cd
khansakhai <kak.khansa@gmail.com>
2024-09-17 17:09:19 +0700

    ci: add GitHub Actions workflow for PWS deployment

--- bungalapak/settings.py
+++ bungalapak/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://khansa-khairunisa31-bungalapak.pbp.cs.ui.ac.id", "https://khansa-khairunisa31-bungalapak.pbp.cs.ui.ac.id"]
