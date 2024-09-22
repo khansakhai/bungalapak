a9eefb7e1923e754f16ed8e3899f8f7b5dfceda1
khansakhai <kak.khansa@gmail.com>
2024-09-22 12:25:09 +0700

    feat: tugas 4 done

--- bungalapak/settings.py
+++ bungalapak/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ht)=+p!b@-*w4*2!=tn)x6kadtvn)-g!a_3(p^6z9_%8l304_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khansa-khairunisa31-bungalapak.pbp.cs.ui.ac.id"]
 
